97b7b296dfda0dbbd38f796c9c9bce3c5beac347
Jordão Macedo <jordao.souza05@gmail.com>
2024-12-16 13:05:56 -0300

    Add year options

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,17 @@ import axios from 'axios';
 
 const yearSelectOptions = [
   { value: null, text: 'Selecione uma opção' },
+  { value: '2013', text: '2013' },
+  { value: '2014', text: '2014' },
+  { value: '2015', text: '2015' },
+  { value: '2016', text: '2016' },
+  { value: '2017', text: '2017' },
+  { value: '2018', text: '2018' },
+  { value: '2019', text: '2019' },
+  { value: '2020', text: '2020' },
+  { value: '2021', text: '2021' },
+  { value: '2022', text: '2022' },
   { value: '2023', text: '2023' },
-  { value: '2024', text: '2024' },
 ];
 
 const selectedYear = ref(null)
