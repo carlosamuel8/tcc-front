2584816940f90d7a0c04b8fb94b56f29df100a3d
Jordão Macedo <jordao.souza05@gmail.com>
2025-01-26 11:34:17 -0300

    Call tab3Component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Tab1Component from '@/components/tabs/Tab1Component.vue';
 import Tab2Component from '@/components/tabs/Tab2Component.vue';
+import Tab3Component from '@/components/tabs/Tab3Component.vue';
 
 </script>
 
@@ -15,7 +16,7 @@ import Tab2Component from '@/components/tabs/Tab2Component.vue';
           <Tab2Component />
         </BTab>
         <BTab title="Mineração de Processos">
-
+          <Tab3Component />
         </BTab>
       </BTabs>
     </BCard>
